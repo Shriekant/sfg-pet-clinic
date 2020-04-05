d1e7fd4d76ef0dd252202f4e811525bc068338b5
Shrikant Kundur <shrikantkundur@gmail.com>
2020-04-05 19:04:13 +0530

    added properties. closes #29

--- pet-clinic-data/src/main/java/com/dfinite/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/dfinite/sfgpetclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public java.lang.String getAddress() {
+        return address;
+    }
+
+    public void setAddress(java.lang.String address) {
+        this.address = address;
+    }
+
+    public java.lang.String getCity() {
+        return city;
+    }
+
+    public void setCity(java.lang.String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
