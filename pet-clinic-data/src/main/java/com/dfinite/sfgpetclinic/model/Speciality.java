f548c09f03a1b50523ebcc660f5c28a6fc9a089a
Shrikant Kundur <shrikantkundur@gmail.com>
2020-04-05 18:53:24 +0530

    added specialities added to vet. closes #28

---
+++ pet-clinic-data/src/main/java/com/dfinite/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.dfinite.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String Description;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
