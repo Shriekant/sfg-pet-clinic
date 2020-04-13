901db42e16255000570361e2de39c8303f13d4b5
Shrikant Kundur <shrikantkundur@gmail.com>
2020-04-13 11:09:03 +0530

    Added project lombok to project, closes #52

--- pet-clinic-data/src/main/java/com/dfinite/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/dfinite/sfgpetclinic/model/Person.java
@@ -1,8 +1,17 @@
 package com.dfinite.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
@@ -12,19 +21,9 @@ public class Person extends BaseEntity {
     @Column(name="last_name")
     private String lastname;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public Person(Long id, String firstName, String lastname) {
+        super(id);
+        this.firstName=firstName;
+        this.lastname=lastname;
     }
 }
