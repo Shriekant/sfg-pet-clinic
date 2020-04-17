af0b3bd48934953c30566bb999fedcd407039518
Shrikant Kundur <shrikantkundur@gmail.com>
2020-04-17 19:06:46 +0530

    upgrade to Junit5, closes #4

--- pet-clinic-web/src/main/java/com/dfinite/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/dfinite/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,13 +1,24 @@
 package com.dfinite.sfgpetclinic;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+
+
     }
 
+    @Test
+    public  void contextLoad()
+    {
+
+    }
 }
